a42e9e4ba9abfdf4861a52ff8ae66dcb4931ff2e
RiteshGitAccount <riteshkaushal.jobs@gmail.com>
2026-08-28 18:55:54 +0530

    till page open in UI

---
+++ PlaywrightDemo/NUnitPlaywright.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Microsoft.Playwright.NUnit;
+using System.Threading.Tasks;
+
+
+namespace PlaywrightDemo;
+public class NUnitPlaywright:PageTest
+{
+    [SetUp]
+    public async Task Setup()
+    {
+        await Page.GotoAsync("http://uitestingplayground.com/sampleapp");
+       
+
+    }
+
+    [Test]
+    public async Task LoginPage()
+    {
+       
+        
+        await Page.ScreenshotAsync(new PageScreenshotOptions
+            {
+                Path = "screenshot.jpg"
+                
+            }
+        );
+
+        await Expect(Page.GetByRole(AriaRole.Button, new() { Name = "Log In" })).ToBeVisibleAsync();
+
+    }
+}
---
+++ PlaywrightDemo/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿// using NUnit.Framework;
+// using Microsoft.Playwright.NUnit;
+// namespace PlaywrightDemo;
+// public class Tests
+// {
+//     [SetUp]
+//     public void Setup()
+//     {
+//     }
+//
+//     [Test]
+//     public async Task FlipKartLoginPage()
+//     {
+//         using var playwright = await Playwright.CreateAsync();
+//         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+//         {
+//             Headless = false
+//         });
+//         var page = await browser.NewPageAsync();
+//         await page.GotoAsync("http://uitestingplayground.com/sampleapp");
+//         await page.ScreenshotAsync(new PageScreenshotOptions
+//             {
+//                 Path = "screenshot.jpg"
+//                 
+//             }
+//         );
+//         await page.FillAsync("UserName", "Password");
+//         await page.FillAsync("Password", "Password");
+//     }
+// }
